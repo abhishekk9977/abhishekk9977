992f10f790d7ee8111e9eea258b9a780826059e2
Abhishek kumar <abhishekk9977@gmail.com>
2022-07-20 21:56:54 +0530

    new_project

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:catalog/core/store.dart';
+import 'package:catalog/pages/cart_page.dart';
+import 'package:catalog/pages/login_page.dart';
+import 'package:catalog/utils/routes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'pages/home_page.dart';
+import 'widgets/themes.dart';
+
+void main() {
+  runApp(VxState(store: MyStore(), child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      debugShowCheckedModeBanner: false,
+      initialRoute: LoginPage.id,
+      routes: {
+        //   "/": (context) => LoginPage(),
+        HomePage.id: (context) => HomePage(),
+        LoginPage.id: (context) => LoginPage(),
+        MyRoutes.cartRoute: (context) => CartPage(),
+      },
+    );
+  }
+}
