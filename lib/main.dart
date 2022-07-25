2f697a3312ef6d671f9dbec62134389f618fa953
Abhishek kumar <abhishekk9977@gmail.com>
2022-07-25 12:44:30 +0530

    new_updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:catalog/core/store.dart';
 import 'package:catalog/pages/cart_page.dart';
@@ -6,6 +7,7 @@ import 'package:catalog/utils/routes.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'pages/home_page.dart';
 import 'widgets/themes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(VxState(store: MyStore(), child: const MyApp()));
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,12 @@
 import 'package:catalog/pages/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:catalog/utils/routes.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'auth.dart';
 
 class LoginPage extends StatefulWidget {
-  static const String id = 'loginPage';
+  static const String id = 'LoginPage';
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -29,105 +31,142 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  late UserCredential user;
+
+  @override
+  void initState() {
+    super.initState();
+    signOutGoogle();
+  }
+
+  void click() {
+    signInWithGoogle().then((user) => {
+          this.user = user,
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => HomePage())),
+          print("Login done successfully!")
+        });
+  }
+
+  Widget googleLoginButton() {
+    return OutlinedButton(
+        onPressed: click,
+        //  shape: RoundedRectangleBorder(
+        //borderRadius: BorderRadius.circular(15)),
+        // splashColor: Colors.grey,
+        //borderSide: BorderSide(color: Colors.grey),
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                Image(
+                    image: AssetImage('assets/images/google_logo.png'),
+                    height: 35),
+                Padding(
+                    padding: EdgeInsets.only(left: 10),
+                    child: Text('Sign in with Google',
+                        style: TextStyle(color: Colors.grey, fontSize: 25)))
+              ],
+            )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: context.canvasColor,
         child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/images/hey.png",
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Text(
-                  "Welcome $name",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
+            child: Form(
+                key: _formKey,
+                child: Column(children: [
+                  Image.asset(
+                    "assets/images/hey.png",
+                    fit: BoxFit.cover,
                   ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 16.0, horizontal: 32.0),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        decoration: InputDecoration(
-                          hintText: "Enter username",
-                          labelText: "Username",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Username cannot be empty";
-                          }
-
-                          return null;
-                        },
-                        onChanged: (value) {
-                          name = value;
-                          setState(() {});
-                        },
-                      ),
-                      TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          hintText: "Enter password",
-                          labelText: "Password",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Password cannot be empty";
-                          } else if (value.length < 6) {
-                            return "Password length should be atleast 6";
-                          }
-
-                          return null;
-                        },
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                      ),
-                      Material(
-                        color: context.theme.buttonColor,
-                        borderRadius:
-                            BorderRadius.circular(changeButton ? 50 : 8),
-                        child: InkWell(
-                          onTap: () => moveToHome(context),
-                          child: AnimatedContainer(
-                            duration: Duration(seconds: 1),
-                            width: changeButton ? 50 : 150,
-                            height: 50,
-                            alignment: Alignment.center,
-                            child: changeButton
-                                ? Icon(
-                                    Icons.done,
-                                    color: Colors.white,
-                                  )
-                                : Text(
-                                    "Login",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "WELCOME",
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 32.0),
+                      child: Column(children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: "Enter username",
+                            labelText: "Username",
                           ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Username cannot be empty";
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            name = value;
+                            setState(() {});
+                          },
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ));
+                        TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: "Enter password",
+                            labelText: "Password",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Password cannot be empty";
+                            } else if (value.length < 6) {
+                              return "Password length should be atleast 6";
+                            }
+
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 40.0,
+                        ),
+                        Material(
+                          color: context.theme.buttonColor,
+                          borderRadius:
+                              BorderRadius.circular(changeButton ? 50 : 8),
+                          child: InkWell(
+                              onTap: () => moveToHome(context),
+                              child: AnimatedContainer(
+                                  duration: Duration(seconds: 1),
+                                  width: changeButton ? 50 : 150,
+                                  height: 50,
+                                  alignment: Alignment.center,
+                                  child: changeButton
+                                      ? const Icon(
+                                          Icons.done,
+                                          color: Colors.white,
+                                        )
+                                      : const Text(
+                                          "Login",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18),
+                                        ))),
+                        ),
+                        const SizedBox(
+                          height: 60,
+                        ),
+                        googleLoginButton(),
+                      ])),
+                ]))));
   }
+
+  void signOutGoogle() {}
 }
